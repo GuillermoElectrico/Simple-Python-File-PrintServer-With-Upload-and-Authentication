e615b66e478abf289e2c45e4d5d975ea30915836
GuillermoElectrico <guillermoelectrico@outlook.com>
2018-10-12 13:35:33 +0200

    first commit

---
+++ settings.py
@@ -0,0 +1,9 @@
+# change these values as you see fit
+# after running `sudo ./install`, the server should run at http://host:port/base_url
+host = '188.213.165.213'
+port = 5000
+base_url = '/tmp/PDF'
+username = 'admin'
+password = 'mypassword'
+
+# for example, with above default values, the server should run at 127.0.0.1:5000/some_preferred_url/
